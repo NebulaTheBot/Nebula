7c8c210b3d429d2d2b44c2560b415f7ffbc7665f
ThatBoiDev <mart.zielman@kpnmail.nl>
2023-01-28 19:12:20 +0100

    paypal

--- src/commands/info/donate.js
+++ src/commands/info/donate.js
@@ -17,13 +17,13 @@ module.exports = class Donate {
                 name: "**Thanks for considering to donate!**",
                 value: [
                     "The Nebula team will use this money for hosting the website and the bot, and paying other bills.",
-                    "We use [Placeholder](https://www.google.com) for donating."
+                    "We use [PayPal](https://www.google.com) for donating."
                 ].join("\n\n")},
                 {
                     name: "**Why should I donate?**",
                     value: [
                         "By donating, you help the Nebula team exist. The Nebula project is completely free and open source, and donating is a great way to help us.",
-                        "If you connect your Placeholder account with Discord, you can get a Donator role and access to donator-only channels in our Discord."
+                        "If you connect your PayPal account with Discord, you can get a Donator role and access to donator-only channels in our Discord."
                     ].join("\n\n")
                 })
             .setThumbnail(client.user.displayAvatarURL())
