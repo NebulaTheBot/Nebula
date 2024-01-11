bc805d391ba6dde682ecdcfbe2fcea44bd550a72
Serge <olgadeeva61@gmail.com>
2024-01-11 09:42:38 +0600

    okay now this is done

--- src/utils/validateURL.ts
+++
@@ -1,4 +0,0 @@
-export default function validateURL(url) {
-  if (/^(http(s):\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/g.test(url)) return true;
-  return false;
-}
