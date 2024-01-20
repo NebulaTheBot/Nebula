3be72f256bd322673534ec41a12e931414be5a67
froxcey <danichen204@gmail.com>
2024-01-21 03:19:31 +0800

    Fix serverboard

--- src/commands/serverboard.ts
+++ src/commands/serverboard.ts
@@ -1,12 +1,17 @@
 import {
-  SlashCommandSubcommandBuilder, ButtonBuilder, ActionRowBuilder,
-  ButtonStyle, ButtonInteraction, type Guild,
-  type ChatInputCommandInteraction
+  SlashCommandSubcommandBuilder,
+  ButtonBuilder,
+  ActionRowBuilder,
+  ButtonStyle,
+  ButtonInteraction,
+  type Guild,
+  type ChatInputCommandInteraction,
+  ComponentType,
 } from "discord.js";
 import { quickSort } from "../utils/quickSort";
 import { serverEmbed } from "../utils/embeds/serverEmbed";
 import { listPublicServers } from "../utils/database/settings";
-import {errorEmbed} from "../utils/embeds/errorEmbed";
+import { errorEmbed } from "../utils/embeds/errorEmbed";
 
 export default class Serverboard {
   data: SlashCommandSubcommandBuilder;
@@ -14,34 +19,44 @@ export default class Serverboard {
     this.data = new SlashCommandSubcommandBuilder()
       .setName("serverboard")
       .setDescription("Shows the servers that have Nebula.")
-      .addNumberOption(option => option
-        .setName("page")
-        .setDescription("The page you want to see.")
+      .addNumberOption((option) =>
+        option.setName("page").setDescription("The page you want to see."),
       );
   }
 
   async run(interaction: ChatInputCommandInteraction) {
     const guildsMapped: Record<string, Guild> = {};
     for (const shownGuild of listPublicServers()) {
-      const guild = interaction.client.guilds.cache.find(guild => (guild.id === `${shownGuild}`))!;
+      const guild = interaction.client.guilds.cache.find(
+        (guild) => (guild.id = shownGuild + ""),
+      )!;
       guildsMapped[`${guild.memberCount}:${guild.id}`] = guild;
     }
 
     if (Object.keys(guildsMapped).length == 0)
-      return interaction.reply({"embeds": [errorEmbed("No public server found")]})
+      return interaction.reply({
+        embeds: [errorEmbed("No public server found")],
+      });
 
     const guildsSorted = quickSort(
-      [...Object.keys(guildsMapped).map(i => Number(i.split(":")[0]))],
+      [...Object.keys(guildsMapped).map((i) => Number(i.split(":")[0]))],
       [[...Object.values(guildsMapped)]],
       0,
-      Object.keys(guildsMapped).length - 1
+      Object.keys(guildsMapped).length - 1,
     )[1]![0].reverse();
 
     const pages = guildsSorted.length;
     const argPage = interaction.options.getNumber("page", false)!;
-    let page = (argPage - 1 <= 0 ? 0 : argPage - 1 > pages ? pages - 1 : argPage - 1) || 0;
+    let page =
+      (argPage - 1 <= 0 ? 0 : argPage - 1 > pages ? pages - 1 : argPage - 1) ||
+      0;
     let guild = guildsSorted[page];
-    let embed = await serverEmbed({ guild, page: page + 1, pages, showInvite: true });
+    let embed = await serverEmbed({
+      guild,
+      page: page + 1,
+      pages,
+      showInvite: true,
+    });
 
     const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
       new ButtonBuilder()
@@ -51,12 +66,17 @@ export default class Serverboard {
       new ButtonBuilder()
         .setCustomId("right")
         .setEmoji("1137330125004869702")
-        .setStyle(ButtonStyle.Primary)
+        .setStyle(ButtonStyle.Primary),
     );
 
-    await interaction.reply({ embeds: [embed], components: [row] });
-    interaction.channel
-      ?.createMessageComponentCollector({ filter: i => i.user.id === interaction.user.id, time: 60000 })
+    const reply = await interaction.reply({
+      embeds: [embed],
+      components: [row],
+    });
+    reply.createMessageComponentCollector({
+        componentType: ComponentType.Button,
+        time: 60000,
+      })
       .on("collect", async (i: ButtonInteraction) => {
         switch (i.customId) {
           case "left":
